89eca48958ca36714d97364141fb041c7682a89e
maciejsudo <maciejsudol99@gmail.com>
2023-09-04 17:53:08 +0200

    not realy working example, but show must go on

--- LSTM.py
+++ LSTM.py
@@ -6,22 +6,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader,  TensorDataset
 import soundfile as sf
 import os
-
-'''
-class WindowArrayDataset(Dataset):
-    def __init__(self, x, y, window_len):
-        self.x = x
-        self.y = y[window_len - 1 :]
-        self.window_len = window_len
-
-    def __len__(self):
-        return len(self.x) - self.window_len + 1
-
-    def __getitem__(self, index):
-        x_out = torch.stack([torch.tensor(self.x[idx: idx + self.window_len]) for idx in range(index, index + 1)]).view(-1, self.window_len)
-        y_out = torch.tensor(self.y[index : index + self.window_len]).view(-1, self.window_len)
-        return x_out, y_out
-'''
+from matplotlib import pyplot as plt
 
 
 
@@ -41,18 +26,6 @@ class WindowArrayDataset(Dataset):
         y_out = torch.tensor(self.y[index*self.batch_size:(index+1)*self.batch_size])
         return x_out, y_out
 
-def my_collate(data_in):
-    X= []
-    Y =[]
-    Z =[]
-
-    for seq in data_in:
-        X.append(seq[0])
-        Y.append(seq[1])
-
-        X = torch.stack(X,dim=1)
-        Y = torch.stack(Y,dim=1)
-        return X,Y
 #class WindowArrayLoader(DataLoader):
 #    def __init__(self, x, y, window_len, batch_size=32, shuffle=False):
 #        dataset = WindowArrayDataset(x, y, window_len)
@@ -97,72 +70,136 @@ def prepare_WAVs(path):
 
     return [X_all, y_all]
 
+def concat_WAV(data, value):
+    '''
+    # data = audio data tensor (23456,1)
+    # value = tine. drive or level param
+    '''
+    audio_len = list(data.size())[0]
+    param = torch.full((audio_len,1), value)
+    return torch.cat((data,param), dim =1)
+
+
+def return_n_data(full_data, div_factor,exact_number):
+    '''
+    # full data = the tensor (audio_values, params)
+    # div_factor = the value dividing the full data size
+    # exact number = [0...div_factor-1]
+    '''
+    full_size = list(full_data.size())[0]
+    a = int(full_size/div_factor)
+    return full_data[a*exact_number : a*(exact_number+1)]
+
+
 def save_wav(name, data):
-    wavfile.write(name, 44100, data.flatten().astype(np.float32))
+      wavfile.write(name, 44100, data.flatten().astype(np.float32))
+
+
 
 if __name__ == "__main__":
 
-    name = 'test1'
+  name = 'test1'
 
-    path = "D:\\repos\MGR\\neural-analog\data\\test\\combined"
-    data_test = prepare_WAVs(path)
+  path = "D:\\repos\MGR\\neural-analog\data\\test\\t20k_l100k_d100k_fs44100_bd16_time180s"
+  data_test = prepare_WAVs(path)
 
-    path = "D:\\repos\MGR\\neural-analog\data\\train\\combined"
-    data_train = prepare_WAVs(path)
+  path = "D:\\repos\MGR\\neural-analog\data\\train\\t20k_l100k_d100k_fs44100_bd16_time180s"
+  data_train = prepare_WAVs(path)
 
-    path = "D:\\repos\MGR\\neural-analog\data\\validation\\combined"
-    data_val = prepare_WAVs(path)
-
-
-    #LSTM#
+  path = "D:\\repos\MGR\\neural-analog\data\\validation\\t20k_l100k_d100k_fs44100_bd16_time180s"
+  data_val = prepare_WAVs(path)
 
 
-    epochs = 1
-    input_size = 150
-    batch_size = 4096
-    test_size = 0.2
-    train_mode = 0
-
-    if train_mode == 0:  # Speed Training
-        learning_rate = 0.01
-        conv1d_strides = 12
-        conv1d_filters = 16
-        hidden_units = 36
-    elif train_mode == 1:  # Accuracy Training (~10x longer than Speed Training)
-        learning_rate = 0.01
-        conv1d_strides = 4
-        conv1d_filters = 36
-        hidden_units = 64
-    else:  # Extended Training (~60x longer than Accuracy Training)
-        learning_rate = 0.0005
-        conv1d_strides = 3
-        conv1d_filters = 36
-        hidden_units = 96
-
-    # Define model:
-    model = GuitarLSTM(conv1d_filters, conv1d_strides, input_size, hidden_units)
-    print('Model architcture: \n',model)
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-    # training loader:
-    train_arr = WindowArrayDataset(data_train[0], data_train[1], input_size, batch_size= batch_size)
-    train_loader = DataLoader(train_arr, batch_size=1, shuffle=False)
-
-    #validation loader:
-    val_arr = WindowArrayDataset(data_val[0], data_val[1], input_size, batch_size= batch_size)
-    val_loader = DataLoader(val_arr, batch_size=1, shuffle=False)
+  #LSTM#
 
 
+  epochs = 20
+  input_size = 150
+  batch_size = 4096
+  test_size = 0.2
+  train_mode = 0
+
+
+
+
+
+
+
+
+
+
+
+
+  if train_mode == 0:  # Speed Training
+      learning_rate = 0.01
+      conv1d_strides = 12
+      conv1d_filters = 16
+      hidden_units = 36
+  elif train_mode == 1:  # Accuracy Training (~10x longer than Speed Training)
+      learning_rate = 0.01
+      conv1d_strides = 4
+      conv1d_filters = 36
+      hidden_units = 64
+  else:  # Extended Training (~60x longer than Accuracy Training)
+      learning_rate = 0.0005
+      conv1d_strides = 3
+      conv1d_filters = 36
+      hidden_units = 96
+
+  # Define model:
+  model = GuitarLSTM(conv1d_filters, conv1d_strides, input_size, hidden_units)
+  print('Model architcture: \n',model)
+
+  loss_fn = nn.MSELoss()
+  optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+  # training loader:
+  train_arr = WindowArrayDataset(data_train[0], data_train[1], input_size, batch_size= batch_size)
+  train_loader = DataLoader(train_arr, batch_size=1, shuffle=False)
+
+  #validation loader:
+  val_arr = WindowArrayDataset(data_val[0], data_val[1], input_size, batch_size= batch_size)
+  val_loader = DataLoader(val_arr, batch_size=1, shuffle=False)
+
+  # architecture:
+  # input posklejany + wektor cyfr => output posklejany
+  #
+  # generate parameters [0...9]
+  # inputs = inputs.view(1 * 4096, input_size, 1) + 1 dimension!
+  # targets = targets.view(1 * 4096, 1)
+  #
+  print(data_train[0].shape)
+  val = 1.0
+  param = torch.full((150,1), val)
+  print(param.shape)
+  print(data_train[0].shape)
+
+  # zaimplementowanie wstepnie w pliku: https://colab.research.google.com/drive/1dzlsXIMGQc_Vr2g8VlR8uwKCL8_uCPHN
+
+
+
+
+
+
+
+'''
 
 
     print_interval = 100
+    plot_interval = 10
+    ###################
+    # for plotting purposes:
+    train_loss_plt =[]
+    val_loss_plt =[]
+    mse_sum_train = 0.0
+    num_batches_train = 0
+    ###################
     # Train Model ###################################################
     for epoch in range(epochs):
         model.train()
         for i, (inputs, targets) in enumerate(train_loader):
 
-            inputs = inputs.view(1 *4096, 150, 1)
+            inputs = inputs.view(1 *4096, input_size , 1)
             targets = targets.view(1 *4096, 1)
 
             outputs = model(inputs)
@@ -171,26 +208,63 @@ if __name__ == "__main__":
             optimizer.step()
             optimizer.zero_grad()
 
-            if (i + 1) % print_interval == 0: # every each 100epochs print and run validation!:
+            mse_sum_train += loss
+            num_batches_train += 1
+
+            # plot last validation loss:
+
+            # if(epoch == epochs):
+            #   val_loss_plt.append(val_loss.item())
+
+
+
+            if (i + 1) % print_interval == 0: # every each 100 batches print and run validation!:
 
                 print(f"Epoch [{epoch + 1}/{epochs}], Batch [{i + 1}/{len(train_arr)}], Loss: {loss.item():.4f}")
 
+        #train_loss_plt.append(loss.item()) # add last loss value to plot
+        average_mse_train = mse_sum_train / num_batches_train
+        train_loss_plt.append(average_mse_train.item())
+
+
+
         # for each epoch calculate validation loss:
-        mse_sum = 0.0
-        num_batches = 0
+        mse_sum_val = 0.0
+        num_batches_val = 0
         model.eval()
         with torch.no_grad():
             for i, (inputs, targets) in enumerate(val_loader):
-                inputs = inputs.view(1 * 4096, 150, 1)
+                inputs = inputs.view(1 * 4096, input_size , 1)
                 targets = targets.view(1 * 4096, 1)
 
                 outputs = model(inputs)
                 val_loss = loss_fn(outputs,targets)
 
-                mse_sum += val_loss
-                num_batches +=1
-            average_mse = mse_sum / num_batches
-            print(f"Average validation MSE: {average_mse:.4f}")
+                mse_sum_val += val_loss
+                num_batches_val +=1
+
+                #plot last validation loss:
+
+                #if(epoch == epochs):
+                #   val_loss_plt.append(val_loss.item())
+            average_mse_val = mse_sum_val / num_batches_val
+            print(f"Average validation MSE: {average_mse_val:.4f}")
+            #val_loss_plt.append(val_loss.item())
+            val_loss_plt.append(average_mse_val.item())
+
+    #AFTER ALL EPOCHS:
+    epochs_axis = [x for x  in range(1, epochs+1)]
+
+
+    plt.plot(epochs_axis, train_loss_plt,label = 'train_loss')
+    plt.plot(epochs_axis, val_loss_plt, label='validation_loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss [MSE]')
+    plt.legend()
+    plt.show()
+
+
+
 
 
 
@@ -218,6 +292,10 @@ if __name__ == "__main__":
         prediction = model(sample_item[0])
 
     # Convert PyTorch tensor to numpy array
+    mse = loss_fn(prediction,sample_item[1])
+
+    print(f"Test file  MSE: {mse:.4f}")
+
     prediction = prediction.numpy()
 
 
@@ -227,3 +305,4 @@ if __name__ == "__main__":
 
 
 
+'''
--- model_architestures/LSTM.py
+++ model_architestures/LSTM.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 
+
 class GuitarLSTM(nn.Module):
     def __init__(self, conv1d_filters, conv1d_strides, input_size, hidden_units):
         super(GuitarLSTM, self).__init__()
@@ -9,7 +10,7 @@ class GuitarLSTM(nn.Module):
         self.conv1d_layer2 = nn.Conv1d(in_channels=conv1d_filters, out_channels=conv1d_filters, kernel_size=self.kernel_size, stride=conv1d_strides, padding=6)
 
         self.lstm_layer = nn.LSTM(input_size=conv1d_filters, hidden_size=hidden_units, batch_first=True)
-        self.dense_layer = nn.Linear(hidden_units, 1)
+        self.dense_layer = nn.Linear(hidden_units, 2)
 
     def forward(self, x):
         x = self.conv1d_layer1(x)
@@ -18,5 +19,25 @@ class GuitarLSTM(nn.Module):
         x = self.dense_layer(x[:, -1, :])
         return x
 
+
+
+# class GuitarLSTM(nn.Module):
+#     def __init__(self, conv1d_filters, conv1d_strides, input_size, hidden_units):
+#         super(GuitarLSTM, self).__init__()
+#         self.kernel_size = 12
+#         self.conv1d_layer1 = nn.Conv1d(in_channels=input_size, out_channels=conv1d_filters, kernel_size=self.kernel_size, stride=conv1d_strides, padding=6)
+#
+#         self.conv1d_layer2 = nn.Conv1d(in_channels=conv1d_filters, out_channels=conv1d_filters, kernel_size=self.kernel_size, stride=conv1d_strides, padding=6)
+#
+#         self.lstm_layer = nn.LSTM(input_size=conv1d_filters, hidden_size=hidden_units, batch_first=True)
+#         self.dense_layer = nn.Linear(hidden_units, 1)
+#
+#     def forward(self, x):
+#         x = self.conv1d_layer1(x)
+#         x = self.conv1d_layer2(x)
+#         x, _ = self.lstm_layer(x.transpose(1, 2))
+#         x = self.dense_layer(x[:, -1, :])
+#         return x
+#
 
 
